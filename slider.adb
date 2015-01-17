7c8c7f50114bc88d0404692aa474d2fe7ff07cd3
John Marino <draco@marino.st>
2015-01-18 00:15:47 +0100

    Bump to version 2.04 for timestamp.
    Note: I can't reproduce the curses crashes at /var/log/(various) anymore
    so either the problem has been (incidently) fixed or it's currently
    dormant.  Either way, Slider seems to be behaving now.

--- slider.adb
+++ slider.adb
@@ -23,7 +23,7 @@ procedure slider is
    package CL  renames Ada.Command_Line;
    package TIO renames Ada.Text_IO;
 
-   SVER : constant String := "2.03";
+   SVER : constant String := "2.04";
 
    procedure echo_usage;
 
