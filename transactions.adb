b25148a0015c44d365799b4ccc6f3fd64b64c6ab
John Marino <draco@marino.st>
2015-01-17 23:47:37 +0100

    Update (c) copyright date on all header files to include 2015

--- transactions.adb
+++ transactions.adb
@@ -1,5 +1,5 @@
 --
---  Copyright (c) 2014 John Marino <draco@marino.st>
+--  Copyright (c) 2014-15 John Marino <draco@marino.st>
 --
 --  Permission to use, copy, modify, and distribute this software for any
 --  purpose with or without fee is hereby granted, provided that the above
